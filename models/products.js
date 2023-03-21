6416f49943ceef6150ae17a4c169f4aaee4fe363
Aryan Taneja <tanejaaryan85@gmail.com>
2023-03-22 02:04:38 +0530

    Added products Schema

---
+++ models/products.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let productSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        trim:true,
+        required:true
+    },
+    img: {
+        type:String,
+        trim: true,
+        default:'/images/dummy.jpg'
+    },
+    price:{
+        type:Number,
+        min: 0,
+        required:true
+    },
+    desc:{
+        type:String,
+        trim:true
+    },
+    reviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
+})
+
+
+let Product = mongoose.model('Product' , productSchema);
+module.exports = Product;
